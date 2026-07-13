09cb59e20091ec339ad933ff2d7170becc02e125
JAZMIN ARIAS BERLIN <49308713@ort>
2026-07-13 12:16:37 -0300

    ns

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path');
 const { obtenerRutaPeatonalSegura } = require('./ruteoService.js');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
